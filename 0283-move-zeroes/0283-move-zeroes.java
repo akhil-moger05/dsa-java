23e77771f396d1cdc2e88bf167722174518efc66
Akhil Moger <akhilmoger0987@gmail.com>
2026-07-11 19:45:21 +0530

    Time: 2 ms (92.23%), Space: 47.8 MB (38.11%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int w = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[w] = nums[i];
+                w++;
+            }
+        }
+        while (w < nums.length) {
+            nums[w] = 0;
+            w++;
+        }
+    }
+}
